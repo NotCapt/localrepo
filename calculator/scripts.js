799df5d9803f34aeca5edf3f094cc96d752f51f9
NotCapt <faaizhussain006@gmail.com>
2026-01-18 20:03:06 +0530

    first working prototype

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -3,25 +3,49 @@ let operation = null;
 let secondNum = null;
 
 const btns = document.querySelectorAll('button');
+const display = document.querySelector('#display');
 
 btns.forEach(button => {
     button.addEventListener('click', (event) => {
-        const value = event.target.id
+        const value = event.currentTarget.id
+        if (value === 'clear') {
+            firstNum = null;
+            secondNum = null;
+            operation = null;
+            display.textContent = '';
+            return;
+        }
+        if (value === '=') {
+            if (firstNum !== null && secondNum !== null && operation !== null) {
+                const result = operate(firstNum, secondNum, operation);
+                firstNum = result;
+                secondNum = null;
+                operation = null;
+            }
+            return;
+        }
+
         const num = Number(value)
         if (Number.isNaN(num)){
             operation = value;
             console.log(operation);
+            display.textContent = operation;
             return;
         }
+        if (operation === null) {
         firstNum = Number(value);
-        console.log(firstNum);
+        display.textContent = firstNum;
+        console.log(`first num: ${firstNum}`);
+        }
+        else {
+            secondNum = Number(value);
+            display.textContent = secondNum;
+            console.log(`second num: ${secondNum}`);
+        }
         
 
     });
 });
-
-
-
 
 function sum(a, b) {
     return a + b;
@@ -35,5 +59,28 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(firstNum, secondNum, operation) {
+    let ans;
+    switch (operation) {
+        case '+':
+            ans = sum(firstNum, secondNum);
+            break;
+        case '-':
+            ans = subtract(firstNum, secondNum);
+            break;
+        case '*':
+            ans =  multiply(firstNum, secondNum);
+            break;
+        case '/':
+            ans =  divide(firstNum, secondNum);
+            break;
+    }
+    display.textContent = ans;
+    return ans;
+}
+
+
+
 
 
